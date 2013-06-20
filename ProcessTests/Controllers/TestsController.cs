36aaee5cd45cde90fb9e784534d8933527b77f62
Marie Martin <mmartin@cuahsi.org>
2013-06-19 22:05:16 -0400

    first check-in

---
+++ ProcessTests/Controllers/TestsController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UnitTests;
+using System.Configuration;
+using ProcessTests.Models;
+
+namespace ProcessTests.Controllers
+{
+    public class TestsController : Controller
+    {
+        private static string TestCasesFile = ConfigurationSettings.AppSettings["TestCasesFile"];
+        private static string ExpectedResponsesFolder = ConfigurationSettings.AppSettings["ExpectedResponsesFolder"];
+        private static string RealtimeResponsesFolder = ConfigurationSettings.AppSettings["RealtimeResponsesFolder"];
+
+        public ActionResult Index()
+        {
+            var result = new vmMain();
+            var load = new vmAjaxResult();
+            try
+            {
+                var listOfServiceCallsToMake = XMLHelpers.ParseTestXML(TestCasesFile);
+                var populatedUrls = XMLHelpers.ConstructServiceUrl(listOfServiceCallsToMake);
+                result.TestCases = new List<vmTestCase>();
+                populatedUrls.ForEach(x =>
+                {
+                    result.TestCases.Add(new vmTestCase { TestSuiteObject = x });
+                });
+                load.ErrorCode = 0;
+                if (!populatedUrls.Any())
+                {
+                    load.ErrorCode = 1001;
+                    load.ErrorMessage = String.Format("ERROR #{0}: Unable to recognize test cases from file [{1}]", load.ErrorCode, TestCasesFile);
+                }
+            }
+            catch (Exception ex)
+            {
+                load.ErrorCode = 1000;
+                load.ErrorMessage = String.Format("ERROR #{0}: {1}", load.ErrorCode, ex.Message);
+            }
+            result.vmAjaxResult = load;
+            return View(result);
+        }
+
+        [HttpPost]
+        public JsonResult RunIndividualTest(string testId, string constructedUrl)
+        {
+            var result = new vmAjaxResult();
+            try
+            {
+                result = Tester.RunTest(ExpectedResponsesFolder, RealtimeResponsesFolder, testId, constructedUrl);
+                return Json(result);
+            }
+            catch (Exception ex)
+            {
+                result.ErrorCode = 5000;
+                result.ErrorMessage = Server.HtmlEncode(String.Format("ERROR #{0}: {1}", result.ErrorCode, ex.Message));
+                return Json(result);
+            }
+
+        }
+
+    }
+}
---
+++ UnitTests/ServiceResponseClasses.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests {
+    public class ServiceResponseClasses {
+        public struct MappedVariable {
+            public string variableName;
+            public string variableCode;
+            public string servCode;
+            public string WSDL;
+            public string conceptKeyword;
+            public string conceptCode;
+        }
+
+        public struct SeriesRecord {
+            public string ServCode;
+            public string ServURL;
+            public string location;
+            public string VarCode;
+            public string VarName;
+            public string beginDate;
+            public string endDate;
+            public string authtoken;
+            public int ValueCount;
+            public string Sitename;
+            public double latitude;
+            public double longitude;
+            public string datatype;
+            public string valuetype;
+            public string samplemedium;
+            public string timeunits;
+            public string conceptKeyword;
+            public string genCategory;
+            public string TimeSupport;
+        }
+
+        public struct ServiceInfo {
+            public string servURL;
+            public string Title, ServiceDescriptionURL;
+            public string name, Email, phone;
+            public string organization, orgwebsite, citation, aabstract;
+            public int valuecount;
+            public int variablecount, sitecount;
+            public int ServiceID;
+            public string NetworkName;
+            public double minx, miny, maxx, maxy;
+            public string serviceStatus;
+        }
+
+        public struct Site {
+            public string SiteName;
+            public string SiteCode;
+            public double Latitude;
+            public double Longitude;
+            public string HUC;
+            public int HUCnumeric;
+            public string servCode;
+            public string servURL;
+        }
+
+
+    }
+}
